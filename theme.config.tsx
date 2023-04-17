c05ba911359474d6cfb17da1ad829d6bd170103c
Bikrant Jung <bikrantjung1214@gmail.com>
2023-04-17 09:00:40 +0545

    Basic Setup

--- theme.config.tsx
+++ theme.config.tsx
@@ -1,18 +1,13 @@
-import React from 'react'
-import { DocsThemeConfig } from 'nextra-theme-docs'
+import React from "react";
+import { DocsThemeConfig } from "nextra-theme-docs";
 
 const config: DocsThemeConfig = {
-  logo: <span>My Project</span>,
+  logo: <span style={{ fontWeight: "bold" }}>Amusing Algorithms</span>,
   project: {
-    link: 'https://github.com/shuding/nextra-docs-template',
+    link: "https://github.com/shuding/nextra-docs-template",
   },
-  chat: {
-    link: 'https://discord.com',
-  },
-  docsRepositoryBase: 'https://github.com/shuding/nextra-docs-template',
-  footer: {
-    text: 'Nextra Docs Template',
-  },
-}
 
-export default config
+  docsRepositoryBase: "https://github.com/shuding/nextra-docs-template",
+};
+
+export default config;
