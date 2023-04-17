22acac5c043c62d0b59ecea2fa3b1205f0c622c2
Bikrant Jung <bikrantjung1214@gmail.com>
2023-04-17 13:16:30 +0545

    Removed Algorithms Folder

--- src/algorithms/recursion/findBinary.js
+++
@@ -1,7 +0,0 @@
-function findBinary(decimal, result) {
-  if (decimal == 0) return result;
-  result = (decimal % 2) + result;
-  return findBinary(Math.floor(decimal / 2), result);
-}
-
-findBinary(45, "");
