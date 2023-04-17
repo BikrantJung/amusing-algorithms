1ac014de64f16b84b0b7251b302d5e65ace03773
Bikrant Jung <bikrantjung1214@gmail.com>
2023-04-17 13:07:30 +0545

    Stacks Data structures

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -2,7 +2,7 @@ import { Html, Head, Main, NextScript } from "next/document";
 
 export default function Document() {
   return (
-    <Html>
+    <Html style={{ scrollBehavior: "smooth" }}>
       <Head>
         <link rel="shortcut icon" href="/static/favicon.png" />
       </Head>
